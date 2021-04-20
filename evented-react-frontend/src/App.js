0100308a1046a2b166bb883e29327b2d566a357e
Gearz <shanegeary@yahoo.com>
2021-04-20 10:34:14 -0600

    added groups routes

--- evented-react-frontend/src/App.js
+++ evented-react-frontend/src/App.js
@@ -1,9 +1,17 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import GroupsIndexContainer from './containers/GroupsIndexContainer';
+import GroupFormContainer from './containers/GroupFormContainer';
 
 function App() {
   return (
     <div className="App">
-        
+        <Router>
+          <Switch>
+            <Route exact path="/">Groups</Route>
+            <Route path="/groups/new">New Group</Route>
+          </Switch>
+        </Router>
     </div>
   );
 }
